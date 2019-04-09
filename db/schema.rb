f3ebd6601db17651c2bd0616e4cc20da94ece22a
Kudzanayi Dzvairo <kudzanayi@Kudzanayis-MacBook-Air.local>
2019-04-09 14:38:05 -0400

    just in case

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_07_145020) do
+ActiveRecord::Schema.define(version: 2019_04_09_145012) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2019_04_07_145020) do
     t.integer "page_count"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
   create_table "user_books", force: :cascade do |t|
